c3e49efcd4c510be0487b9e4793796a0b2d507b8
Tia <49786642+soft-nougat@users.noreply.github.com>
2021-01-22 14:55:19 +0100

    clean up ner.py, add some comments

--- ner.py
+++ ner.py
@@ -2,6 +2,11 @@
 """
 Created on Wed Dec 30 14:40:59 2020
 
+NER script contains code for NER extraction with spacy
+The process is 2-fold:
+    1. Plot NERs extracted by spacy and get unique NER instances
+    2. Plot specific NER instances in text
+
 @author: TNIKOLIC
 """
 import streamlit as st
@@ -10,6 +15,7 @@ from collections import  Counter
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# visualise extracted NER count and extract NERs
 def plot_named_entity_barchart(text):
     nlp = spacy.load("en_core_web_sm")
     
@@ -30,33 +36,34 @@ def plot_named_entity_barchart(text):
           
     plt.title('NER instance count\n\n', 
               fontweight ="bold")
-    
+
+    # show plot in streamlit
     st.pyplot()
     
-    # get unique list elements
+    # get unique list elements to pass to
+    # streamlit app for dropdown options
     ent = set(ent)
     
     return ent
     
 # visualise tokens per entity
 def plot_most_common_named_entity_barchart(text, entity):
+   
+    nlp = spacy.load("en_core_web_sm")
+
+    def _get_ner(text,ent):
+        doc=nlp(text)
+        return [X.text for X in doc.ents if X.label_ == ent]
     
+    entity_filtered=text.apply(lambda x: _get_ner(x,entity))
+    entity_filtered=[i for x in entity_filtered for i in x]
         
-        nlp = spacy.load("en_core_web_sm")
+    counter=Counter(entity_filtered)
+    x,y=map(list,zip(*counter.most_common()))
+    sns.barplot(y,x).set_title(entity)       
         
-        def _get_ner(text,ent):
-            doc=nlp(text)
-            return [X.text for X in doc.ents if X.label_ == ent]
+    st.pyplot() 
     
-        entity_filtered=text.apply(lambda x: _get_ner(x,entity))
-        entity_filtered=[i for x in entity_filtered for i in x]
-        
-        counter=Counter(entity_filtered)
-        x,y=map(list,zip(*counter.most_common()))
-        sns.barplot(y,x).set_title(entity)
-              
-        
-        st.pyplot()
     
 
     
