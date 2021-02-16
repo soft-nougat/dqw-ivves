8c25aceab3314096f070eff06b306e08f6bdad60
Tia <49786642+soft-nougat@users.noreply.github.com>
2021-02-16 15:41:20 +0100

    try to design my own download button

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ import base64
 import SessionState
 # Load the LDA model from gensim
 import preprocessor as pp
+import pickle
+import uuid
+import re
 
 # ----------------------------------------------
 # session state
@@ -196,6 +199,82 @@ def check_input_method(data_input_mthd):
     
     return df,ss.txt
 
+def download_button(object_to_download, download_filename, button_text, pickle_it=False):
+    """
+    Generates a link to download the given object_to_download.
+    Params:
+    ------
+    object_to_download:  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv,
+    some_txt_output.txt download_link_text (str): Text to display for download
+    link.
+    button_text (str): Text to display on download button (e.g. 'click here to download file')
+    pickle_it (bool): If True, pickle file.
+    Returns:
+    -------
+    (str): the anchor tag to download object_to_download
+    Examples:
+    --------
+    download_link(your_df, 'YOUR_DF.csv', 'Click to download data!')
+    download_link(your_str, 'YOUR_STRING.txt', 'Click to download text!')
+    """
+    if pickle_it:
+        try:
+            object_to_download = pickle.dumps(object_to_download)
+        except pickle.PicklingError as e:
+            st.write(e)
+            return None
+
+    else:
+        if isinstance(object_to_download, bytes):
+            pass
+
+        elif isinstance(object_to_download, pd.DataFrame):
+            object_to_download = object_to_download.to_csv(index=False)
+
+        # Try JSON encode for everything else
+        else:
+            object_to_download = json.dumps(object_to_download)
+
+    try:
+        # some strings <-> bytes conversions necessary here
+        b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    except AttributeError as e:
+        b64 = base64.b64encode(object_to_download).decode()
+
+    button_uuid = str(uuid.uuid4()).replace('-', '')
+    button_id = re.sub('\d+', '', button_uuid)
+
+    custom_css = f""" 
+        <style>
+            #{button_id} {{
+                background-color: rgb(255, 255, 255);
+                color: rgb(38, 39, 48);
+                padding: 0.25em 0.38em;
+                position: relative;
+                text-decoration: none;
+                border-radius: 4px;
+                border-width: 1px;
+                border-style: solid;
+                border-color: rgb(230, 234, 241);
+                border-image: initial;
+            }} 
+            #{button_id}:hover {{
+                border-color: rgb(246, 51, 102);
+                color: rgb(246, 51, 102);
+            }}
+            #{button_id}:active {{
+                box-shadow: none;
+                background-color: rgb(246, 51, 102);
+                color: white;
+                }}
+        </style> """
+
+    dl_link = custom_css + f'<a download="{download_filename}" id="{button_id}" href="data:file/txt;base64,{b64}">{button_text}</a><br></br>'
+
+    return dl_link
+
 # app setup 
 try:
     
@@ -292,14 +371,13 @@ try:
             # final step
             download=st.button('Click here to download clean data')
             if download:
-              'Download Started!'
-              df_download= pd.DataFrame(data)
-              #df_download.columns=[text_column]
-              #df_download
-              csv = df_download.to_csv(index=False)
-              b64 = base64.b64encode(csv.encode()).decode()  # some strings
-              linko= f'<a href="data:file/csv;base64,{b64}" download="clean_data.csv">Download csv file</a>'
-              st.markdown(linko, unsafe_allow_html=True)
+              #df_download= pd.DataFrame(data)
+              #csv = df_download.to_csv(index=False)
+              #b64 = base64.b64encode(csv.encode()).decode()  # some strings
+              #linko= f'<a href="data:file/csv;base64,{b64}" download="clean_data.csv">Download csv file</a>'
+              #st.markdown(linko, unsafe_allow_html=True)
+              download_button_str = download_button(data, "dqw_clean_data", "Download clean file")
+              st.markdown(download_button_str, unsafe_allow_html=True)
               
     
     st.subheader('A preview of input data is below, please select plot to start analysis :bar_chart:')
