fc98106eb9c16d07b1eb8f23585530e369e7e7b5
Tia <49786642+soft-nougat@users.noreply.github.com>
2022-01-31 13:50:46 +0100

    fix wkhtmltopdf error

--- tabular_eda/structured_data.py
+++ tabular_eda/structured_data.py
@@ -12,6 +12,10 @@ import pdfkit as pdf
 from fpdf import FPDF
 import io
 
+# remove before commiting
+path_wkhtmltopdf = r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe'
+config = pdf.configuration(wkhtmltopdf=path_wkhtmltopdf)
+
 def structured_data_app():
 
     # ----------------------------------------------
@@ -164,7 +168,7 @@ def create_pdf_html(html, header_text, file_name):
     Funstion to create a pdf from an html file
     """
     # create a pdf file
-    pdf_report = pdf.from_file(html)
+    pdf_report = pdf.from_file(html, configuration = config)
 
     # option to download in app
     display_app_header(main_txt = header_text,
