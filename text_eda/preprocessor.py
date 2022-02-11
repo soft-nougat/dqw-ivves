a40914624e064340a2fd77e9b51856c6824310bc
Tia <49786642+soft-nougat@users.noreply.github.com>
2022-02-11 16:07:34 +0100

    improve the preprocessor

--- text_eda/preprocessor.py
+++ text_eda/preprocessor.py
@@ -6,7 +6,6 @@ https://www.kdnuggets.com/2018/03/text-data-preprocessing-walkthrough-python.htm
 """
 import streamlit as st
 import warnings
-warnings.filterwarnings("ignore",category=DeprecationWarning)
 import re, unicodedata
 import nltk
 import contractions
@@ -14,34 +13,30 @@ import inflect
 from nltk.stem import LancasterStemmer, WordNetLemmatizer
 from nltk.corpus import stopwords
 from textblob import TextBlob
-nltk.download('wordnet')
-nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('omw-1.4')
+from bs4 import BeautifulSoup
+import spacy
+
+def remove_html(words):
+    # language agnostic
+    soup = BeautifulSoup(words, 'lxml')
+    clean_words = soup.get_text()
+    return clean_words
+
 
 def remove_non_ascii(words):
     """Remove non-ASCII characters from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = unicodedata.normalize('NFKD', word).encode('ascii', 'ignore').decode('utf-8', 'ignore')
-        new_words.append(new_word)
+    new_words = [unicodedata.normalize('NFKD', w).encode('ascii', 'ignore').decode('utf-8', 'ignore') for w in words]
     return new_words
 
 def to_lowercase(words):
     """Convert all characters to lowercase from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = word.lower()
-        new_words.append(new_word)
+    new_words = [word.lower() for word in words]
     return new_words
 
 def remove_punctuation(words):
     """Remove punctuation from list of tokenized words"""
-    new_words = []
-    for word in words:
-        new_word = re.sub(r'[^\w\s]', '', word)
-        if new_word != '':
-            new_words.append(new_word)
+    new_words = [re.sub(r'[^\w\s]', '', word) for word in words]
+
     return new_words
 
 def replace_numbers(words):
@@ -76,22 +71,65 @@ def stem_words(words):
 def lemmatize_verbs(words):
     """Lemmatize verbs in list of tokenized words"""
     lemmatizer = WordNetLemmatizer()
-    lemmas = []
-    for word in words:
-        lemma = lemmatizer.lemmatize(word, pos='v')
-        lemmas.append(lemma)
+
+    lemmas = [lemmatizer.lemmatize(i, pos="a") for i in words]
+
+    lemmas = " ".join(lemmas)
+
     return lemmas
 
 def normalize(words):
+    #de-noising objects
+    url_pattern = re.compile(re.compile(r'https?://\S+|www\.S+'))
+    email_pattern = re.compile(re.compile(r'\S*@\S*\s?'))
+
+    words = [url_pattern.sub('', w) for w in words]
+    words = [email_pattern.sub('', w) for w in words]
+    
     words = remove_non_ascii(words)
+    
     words = to_lowercase(words)
+    
     words = remove_punctuation(words)
+
     words = replace_numbers(words)
+   
     words = remove_stopwords(words)
-    
+
     return words
-    
+
+
 def clean_data(df,feature):
+    """
+    function to:
+        1. de-noise
+            a) remove email and other symbols
+        2. tokenize
+        3. normalize
+            a) remove stopwords with NLTK
+            b) lemmatize with NLTK
+            c) replace numbers with words 
+    output: pandas dataframe
+
+    """
+    nlp = spacy.load("xx_ent_wiki_sm")
+
+    df = df.dropna()
+
+    # use lambda function to run through rows (entries)
+    df[feature] = df[feature].apply(lambda x: contractions.fix(x))
+
+    df[feature] =  df[feature].apply(lambda x: nltk.word_tokenize(x))
+
+    df[feature] = df[feature].apply(lambda x: normalize(x))
+
+    df[feature] = df[feature].apply(lambda x: lemmatize_verbs(x))
+
+    st.write(df[feature])
+     
+    return df
+    
+def clean_data_1(df,feature):
     """
     function to:
         1. de-noise
